eca6e95e019b0cdf19ca3fff4a64934a44f94f6b
canelo <chyg201672@gmail.com>
2024-11-11 19:40:59 +0900

    feat: 관리자 공지사항 관리 페이지 퍼블리싱

---
+++ thiscoding/app/(components)/admin/notices/Notices.tsx
@@ -0,0 +1,35 @@
+import "@/app/globals.css"
+import NoticesContent from "./NoticesContent";
+import Link from "next/link";
+const Notices = () => {
+  return (
+    <>
+      <>
+        <div className="w-[950] h-[650] bg-[#FFFFFF] ml-24 rounded-2xl border-[#EBEBEB] relative">
+          <div className="absolute flex justify-end mt-3 right-[50] w-full font-thin">개별 문의 : abc123@naver.com</div>
+          <table className="w-[850] ml-[50] mt-12 mr-[50]">
+            <thead>
+              <tr className="border-t border-b border-black h-[50px]">
+                <th className="px-4 py-2 align-middle">카테고리</th>
+                <th className="px-4 py-2 align-middle w-[380]">제목</th>
+                <th className="px-4 py-2 align-middle w-[200]">날짜</th>
+                <th className="px-4 py-2 align-middle">조회수</th>
+                <th className="px-4 py-2 align-middle">삭제</th>
+              </tr>
+            </thead>
+            <tbody>
+              <NoticesContent />
+              <NoticesContent />
+              <NoticesContent />
+              <NoticesContent />
+            </tbody>
+          </table>
+          <Link href="#" className="w-[90] h-[40] border rounded-2xl flex justify-center items-center border-black absolute right-[50] bottom-[30] text-[#0095E8]">
+            글쓰기
+          </Link>
+        </div>
+      </>
+    </>
+  )
+}
+export default Notices;
