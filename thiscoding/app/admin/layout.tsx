d61d444d37ef8c98ed34b86395bbafe7d9052dbb
canelo <chyg201672@gmail.com>
2024-11-11 18:57:57 +0900

    feat:관리자 신고 페이지 퍼블리싱

---
+++ thiscoding/app/admin/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "관리자 페이지",
+  description: "관리자 메인 페이지",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
