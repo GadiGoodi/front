0188f863c679b5432e30021d6718f9e99f075ee5
canelo <chyg201672@gmail.com>
2024-11-12 14:44:44 +0900

    feat:마이페이지 북마크한 QnA 페이지 퍼블리싱

---
+++ thiscoding/app/(components)/mypage/bookmark/BookMarkQnA.tsx
@@ -0,0 +1,17 @@
+import "@/app/globals.css"
+import BookMarkQnAContent from "./BookMarkQnAContent";
+const BookMarkQnA = () => {
+  return (
+    <>
+      <div className="w-[950] h-[650] bg-[#FFFFFF] ml-24 rounded-2xl border-[#EBEBEB]">
+        <div className="mb-12" />
+        <div className="border-b border-black mx-[50]" />
+        <BookMarkQnAContent />
+        <BookMarkQnAContent />
+        <BookMarkQnAContent />
+        <BookMarkQnAContent />
+      </div>
+    </>
+  )
+}
+export default BookMarkQnA;
