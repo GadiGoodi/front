1ad61cfe0b42aadd6c86a06dd428bf1e009d1562
jong1uk <106105315+jong1uk@users.noreply.github.com>
2024-11-14 06:08:13 +0900

    feat: 채팅 기능

---
+++ thiscoding/app/(components)/chat/Messenger.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import FriendList from './FriendList';
+import ChatList from './ChatList';
+import ChatRoom from './ChatRoom';
+
+const Messenger = () => {
+  const [currentState, setCurrentState] = useState<string>('FriendList');
+  const [previousState, setPreviousState] = useState<string>('');
+
+  const handleStateChange = (newState: string) => {
+    setPreviousState(currentState); // 현재 상태를 이전 상태로 저장
+    setCurrentState(newState);
+  };
+
+  const handleChatSelect = () => {
+    handleStateChange('ChatRoom');
+  };
+
+  const handleFriendSelect = () => {
+    handleStateChange('ChatRoom');
+  };
+
+  const handleBack = () => {
+    setCurrentState(previousState); // 이전 상태로 돌아가기
+  };
+
+  return (
+    <div className="w-full h-full flex flex-col">
+      <div className="w-[247px] h-[324px] bg-[#fafafa] rounded-xl fixed bottom-[190px] right-[40px] drop-shadow-md">
+        {currentState === 'FriendList' && (
+          <FriendList
+            onFriendClick={handleChatSelect}
+            onChangeState={handleStateChange}
+          />
+        )}
+        {currentState === 'ChatList' && (
+          <ChatList
+            onChatSelect={handleChatSelect}
+            onChangeState={handleStateChange}
+          />
+        )}
+        {currentState === 'ChatRoom' && <ChatRoom onBack={handleBack} />}
+      </div>
+      <div className="w-0 h-0 border-solid border-t-[35px] border-r-[15px] border-b-0 border-l-[15px] border-t-[#fafafa] border-r-transparent border-b-transparent border-l-transparent fixed bottom-[155px] right-[65px] drop-shadow-sm"></div>
+    </div>
+  );
+};
+
+export default Messenger;
