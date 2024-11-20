c6f5578e1de2484f3d14cfcdba9507aeca789329
kimsiun <157556686+kimsiun@users.noreply.github.com>
2024-11-21 04:03:03 +0900

    feat: 채팅, top 플로팅 버튼

--- thiscoding/app/mypage/layout.tsx
+++ thiscoding/app/mypage/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "ThisCoding MyPage",
-  description: "마이페이지 사용자 정보 페이지",
+  title: 'ThisCoding MyPage',
+  description: '마이페이지 사용자 정보 페이지',
 };
 
 export default function RootLayout({
@@ -12,9 +12,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
