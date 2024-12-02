d3e62631c89518ec752e63007e510cf76e24ca57
canelo <chyg201672@gmail.com>
2024-12-02 17:38:20 +0900

    feat:관리자 공지사항 삭제 기능 구현

--- thiscoding/app/(components)/admin/adminNotices/AdminNoticesContent.tsx
+++ thiscoding/app/(components)/admin/adminNotices/AdminNoticesContent.tsx
@@ -1,26 +1,41 @@
+import { AdminNoticesListDTO } from "@/app/(models)/admin/adminNotices/adminNoticesListDTO";
 import "@/app/globals.css"
 import DeleteIcon from '@mui/icons-material/Delete';
-const AdminNoticesContent = () => {
-
+import axios from "axios";
+import { useRouter } from "next/navigation";
+const AdminNoticesContent = ({ notices }: { notices: AdminNoticesListDTO }) => {
+  const router = useRouter();
 
   const deleteNotices = () => {
-    const removeNotices = () => {
-      //공지사항 삭제 api
-    }
+    const result = axios.delete(`http://localhost:8080/api/admin/notices/${notices.id}`)
+      .then(res => {
+        console.log(res);
+        router.push("/admin/notices?page=0");
+      }).catch(err => (
+        console.log(err)
+      ))
+    return result;
+  }
+
+  const NoticesDetailRouter = () => {
+    router.push(`/admin/notices/${notices.id}`)
   }
 
   return (
-    <tr className="border-b border-black h-[50]">
-      <td className="flex justify-center items-center h-[50] text-center">
-        <div>
-          <div className="flex justify-center items-center rounded-full w-[80] h-[30] bg-[#444444] text-white">FAQ</div>
-        </div>
-      </td>
-      <td className="text-center">자주 묻는 질문</td>
-      <td className="text-center">2024-11-11 19:17:24</td>
-      <td className="text-center">4738</td>
-      <td className="text-center"><DeleteIcon onClick={() => deleteNotices()} /></td>
-    </tr>
+    <>
+      <tr className="border-b border-black h-[50]" onClick={NoticesDetailRouter}>
+        <td className="flex justify-center items-center h-[50] text-center">
+          <div>
+            <div className="flex justify-center items-center rounded-full w-[80] h-[30] bg-[#444444] text-white">{notices.category}</div>
+          </div>
+        </td>
+        <td className="text-center">{notices.title}</td>
+        <td className="text-center">2024-11-11 19:17:24</td>
+        <td className="text-center">4738</td>
+        <td className="text-center"><DeleteIcon onClick={() => deleteNotices()} /></td>
+      </tr>
+    </>
   );
+
 }
 export default AdminNoticesContent;
